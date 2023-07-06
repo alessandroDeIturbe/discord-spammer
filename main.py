fa0ac784eb404f73fcbc669c81571c87b04d5fca
alessandrodeiturbe <aledei2250@gmail.com>
2023-07-06 23:58:48 +0200

    optimized code

--- main.py
+++ main.py
@@ -2,71 +2,81 @@ import os
 import requests
 import time
 
-ac_token = ''
-message = ''
-tts = 1
-tts_value = ''
-cid = ''
 
-choice = int(input('What do you want to do? (0 = Send message, 1 = Quit): '))
-if choice == 0:
+def get_token():
     if os.path.exists('token.key'):
         with open('token.key', 'r') as file:
-            ac_token = file.read()
-            
-        if ac_token == '':
+            token = file.read().strip()
+
+        if not token:
             print('Invalid token in token.key')
             exit(1)
-        
-        print('TOKEN: {}'.format(ac_token))
-           
+
     else:
-        ac_token = input('Insert your account token: ')
-        print('Is better to store your token in a \'token.key\' file. Create it?')
+        token = input('Insert your account token: ')
+        print('It is better to store your token in a \'token.key\' file. Create it?')
         create = input('[y]/n: ')
         if create.lower() == 'y' or create == '':
             with open('token.key', 'w') as file:
-                file.write(ac_token)
+                file.write(token)
 
-        else:
-            pass
-        
-    while True:
-        cid = input('Insert the channel ID: ')
+    return token
+
+
+def send_message(token, channel_id, message, tts):
+    headers = {
+        "authorization": token
+    }
+
+    data = {
+        "content": message,
+        "tts": str(bool(tts))
+    }
+
+    url = f'https://discord.com/api/v9/channels/{channel_id}/messages'
+
+    try:
+        response = requests.post(url, data=data, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(str(e))
+        print('Stopping process for 10 seconds...')
+        time.sleep(10)
+
+
+def main():
+    token = get_token()
+    choice = int(
+        input('What do you want to do? (0 = Send message, 1 = Quit): '))
+
+    if choice == 0:
+
+        channel_id = input('Insert the channel ID: ')
         message = input('Insert the text: ')
-        tts = int(input('The message must be tts or not? (0 = Yes, 1 = No): '))
-        if tts == 0:
-            tts_value = 'true'
-
-        elif tts == 1:
-            tts_value = 'false'
-
-        else:
-            print('Insert a valid option')
-
-        headers = {
-            "authorization": f"{ac_token}"
-        }
-
-        data = {
-            "content": f"{message}",
-            "tts": f"{tts_value}"
-        }
-
-        url = f'https://discord.com/api/v9/channels/{cid}/messages'
-
+        tts = int(input('Should the message be TTS or not? (0 = Yes, 1 = No): '))
         loop = int(input("Loop? (0 = Loop, 1 = Repeat, 2 = One Message): "))
+
         if loop == 0:
             while True:
+                try:
+                    send_message(token, channel_id, message, tts)
+                    print('Message sent successfully!')
+                except Exception as e:
+                    print('Failed to send message:', str(e))
+        else:
+            repeats = int(input('How many times? ')) if loop == 1 else 1
+            for _ in range(repeats):
                 time.sleep(0.8)
-                r = requests.post(url, data=data, headers=headers)
-        if loop == 1:
-            repeats = int(input('How many times? '))
-            for repeat in range(repeats):
-                time.sleep(0.8)
-                r = requests.post(url, data=data, headers=headers)
-        if loop == 2:
-            r = requests.post(url, data=data, headers=headers)
 
-elif choice == 1:
-    print('See you next time!')
+                try:
+                    send_message(token, channel_id, message, tts)
+                    print('Message sent successfully!')
+                except Exception as e:
+                    print('Failed to send message:', str(e))
+
+    elif choice == 1:
+        print('See you next time!')
+
+
+if __name__ == '__main__':
+    main()
